f3dd4c3c9334d85c1c38e6614db4d32cf4abee62
mateu <mateusz.rostalski@gmail.com>
2018-10-26 15:33:13 +0200

    initial commit

---
+++ src/main/java/pl/mr/demoapp/ServletInitializer.java
@@ -0,0 +1,15 @@
+package pl.mr.demoapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(DemoappApplication.class) ;
+    }
+
+}
---
+++ src/main/java/pl/mr/demoapp/mainController/MainPageController.java
@@ -0,0 +1,17 @@
+package pl.mr.demoapp.mainController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.ws.rs.GET;
+
+@Controller
+public class MainPageController {
+
+    @GET
+    @RequestMapping(value = {"/" , "/index"})
+    public String showMainPage(){
+        return "index" ;
+    }
+
+}
